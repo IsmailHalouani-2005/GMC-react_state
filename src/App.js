d77e82e619868a0c9be455d917c7d3522e4fb11c
IsmailHalouani-2005 <ismail.halouani@gmail.com>
2021-07-25 17:30:55 +0200

    'State'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor() {
+    super()
 
+  }
+  state = {
+    Person:{
+      fullName: 'Ismail Halouani',
+      bio:'A web designer',
+      imgSrc:'',
+      profession:''
+    },
+    boolean: true,
+    timer: 0,
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+    alert(this.state.timer)
+  }
+  tick() {
+    this.setState(state => ({
+      timer: state.timer + 1
+    }));
+  }
+
+  render() {
+    let styler = {color: 'black', fontFamily:'Montserrat', visibility: 'visible'}
+    const btnStyle = {color: 'white', backgroundColor:'#0763e5', borderRadius:'2em', border:'transparent', padding:'.75em 1.25em', marginTop:'1.5rem', fontSize:'1em', cursor: 'pointer', fontFamily:'Montez'}
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {(this.state.boolean) ? (          
+          <div className="profile" style={styler}>
+                {this.state.Person.imgSrc}
+                <h1 className="name">{this.state.Person.fullName}</h1>
+                <p className="bio">{this.state.Person.bio}</p>
+                <span className="pro">{this.state.Person.profession}</span>
+          </div>) : ("")}
+          <button className="btn" onClick={() => this.setState({boolean: !this.state.boolean})} style={btnStyle} >Show hide</button>
+        </header>
+      </div>
+    )};
+};
 export default App;
